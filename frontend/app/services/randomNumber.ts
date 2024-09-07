24173fa4830303a88256111e8865fc09d0d6b353
BDeuDev <bdeusich.dev@gmail.com>
2024-09-07 00:18:49 -0300

    fix(components) unused variable

--- frontend/app/services/randomNumber.ts
+++ frontend/app/services/randomNumber.ts
@@ -4,7 +4,7 @@ import { baseUrl } from '../config/config';
 export const randomNumber = async () => {
 
     try {
-        const response = await axios.post(`${baseUrl}/random`);
+        const response = await axios.post(`${baseUrl}random`);
         
         return response
     } catch (error) {
