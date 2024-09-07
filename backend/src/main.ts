6816e9625246d5ae8939af51c48938e07071a88e
BDeuDev <bdeusich.dev@gmail.com>
2024-09-07 01:04:30 -0300

    fix(main) cors error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: ['http://localhost:3001'],
+    origin: ['https://roulette-frontend-topaz.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   };
